e97a238d7935b53b0b53a548dbeef8b573022765
jorgegdev <polloandres82@gmail.com>
2024-03-22 12:40:07 +1300

    Final proyect

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'www.JorgeGDev.com',
             {
                 font: font,
                 size: 0.5,
